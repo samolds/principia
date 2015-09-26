531441ae8995f07bea38299263c4335205e85058
Matthew Turner <matthew.turner@utah.edu>
2015-09-26 10:05:39 -0600

    Updated app.js to save state.

--- PhysicsTest/js/app.js
+++ PhysicsTest/js/app.js
@@ -15,7 +15,7 @@ requirejs.config({
 		{
 			name: 'physicsjs',
 			location: 'physicsjs-0.7.0',
-			main: 'physicsjs-full.min'
+			main: 'physicsjs-full'
 		}
 	]
 });
@@ -28,7 +28,7 @@ require([
 		function($, Physics) {
 			 
 			Physics(function(world){			  
-
+			world.timestep(1);
 			var viewWidth = 200;
 			var viewHeight = 200;
 					
@@ -44,31 +44,62 @@ require([
 			  
 			// render on each step
 			world.on('step', function(){ world.render(); });
-			  
-			  			  
+			  						
 			// add a square
 			var square = Physics.body('rectangle', {
-					x: 10, // x-coordinate
-					y: 10, // y-coordinate	
-					vx: 0.4,
+					x: 10, // x-coordinate COM
+					y: 10, // y-coordinate COM
+					vx: 1,
+					vy: 1,
 					width: 20,
 					height: 20
 			});			
-			world.add(square);
+			world.add(square);			
 
 			// add some gravity
-			world.add( Physics.behavior('constant-acceleration') );
+			//world.add( Physics.behavior('constant-acceleration') );
 
 			// bounds of the window
 			var bounds = Physics.aabb(0, 0, viewWidth, viewHeight);			  
 			world.add( Physics.behavior('edge-collision-detection', { aabb: bounds, restitution: 0.5 }) );			
 			// ensure objects bounce when edge collision is detected
 			world.add( Physics.behavior('body-impulse-response') );
-			  
-			// subscribe to ticker to advance the simulation
-			Physics.util.ticker.on(function( time, dt ){ world.step( time ); });
+			
+			var states = [];			
+			world.step();
+			for(var i=0; i<1000; i++)
+			{
+				var curState = world.getBodies()[0].state;
+				var acc = curState.acc.clone();
+				var vel = curState.vel.clone();
+				var pos = curState.pos.clone();
+				var ang = {
+					"acc": curState.angular.acc,
+					"vel": curState.angular.vel,
+					"pos": curState.angular.pos
+				};
+				var saveState = 
+				{
+					"acc": acc,
+					"vel": vel,
+					"pos": pos,
+					"angular": ang
+				};
+				states.push(saveState);
+				world.step();
+			}
+			
+			var slider = document.getElementById('slider');
+			slider.addEventListener("input",function(){
+				document.querySelector('output').innerHTML = slider.value;
+				world.getBodies()[0].state = states[slider.value];
+				world.render();
+			});			
 
-			// start the ticker
-			Physics.util.ticker.start();
+			// Line 5656 for step function
+			// Ideally, we would step to "frame n"
+			// which is the state of the simulation at n*dt world timestep
+			// Step with no arguments should advance to next frame
+			
 		});
 });
