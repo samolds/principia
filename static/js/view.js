4607d5410bb41f4eba1dd0af06a6f1bb3ce89a3c
Danny <deman599@gmail.com>
2015-12-07 02:36:44 -0700

    updated layout. added toolbox to tabs. starting to working on comments section

--- static/js/view.js
+++ static/js/view.js
@@ -2,9 +2,15 @@
   $(document).ready(function(){
     $('ul.tabs').tabs('select_tab', 'tab_id');
   });
-        
+    
 // function tabColor() 
 // {
-//     document.getElementById("simulation-name").blur();
-//     document.getElementById("simulation-name-label").style.display = "none";
+//     var all_tabs = document.getElementsByClassName("property-tab ");
+//     var i;
+//     for (i=0; i<all_tabs.length; i++) {
+//       all_tabs[i].style.backgroundColor = "white";
+//     }
+
+//     var active_tab = document.getElementsByClassName("property-tab active");
+//     active_tab[0].style.backgroundColor = "red";
 // }
