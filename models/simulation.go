a730769ba979a4e0c6a374114f9ebcd7d9402463
Matthew Turner <matthew.turner@utah.edu>
2015-12-08 09:19:33 -0700

    persistence update

--- models/simulation.go
+++ models/simulation.go
@@ -4,7 +4,7 @@ import ()
 
 type Simulation struct {
 	Name     string
-	Contents string
+	Contents string `datastore:",noindex"`
 	UserID   string
 	// Tell datastore to ignore thie field
 	Id int64 `datastore:"-"`
