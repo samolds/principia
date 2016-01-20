949ed14a327ca9b8ae8e755847f6a950ae2fd7a9
daltonwallace <mivec9@gmail.com>
2016-01-20 11:35:29 -0700

    Readding gorilla mux 404 comments

--- routes.go
+++ routes.go
@@ -27,4 +27,9 @@ func init() {
 	router.HandleFunc("/faqs", controllers.FaqsHandler)
 	router.HandleFunc("/feedback", controllers.FeedbackHandler)
 	router.HandleFunc("/comment", controllers.CommentHandler)
+
+	// This is the correct way to handle a 404 error with gorilla mux..
+  // http://stackoverflow.com/questions/9996767/showing-custom-404-error-page-with-standard-http-package
+  // https://medium.com/@matryer/the-http-handlerfunc-wrapper-technique-in-golang-c60bf76e6124#.x29aysovx
+  //router.NotFoundHandler = http.HandlerFunc(controllers.ERRRRRR)
 }
