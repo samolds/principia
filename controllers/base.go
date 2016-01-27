440f1ec35c119ddc1d12916953d182c0568c6d24
daltonwallace <mivec9@gmail.com>
2016-01-27 10:23:37 -0700

    Adding basic jasmine and go unit testing capabilities

--- controllers/base.go
+++ controllers/base.go
@@ -17,6 +17,7 @@ var (
 	dormDir = tmplDir + "dormant/"
 	simDir  = tmplDir + "simulator/"
 	userDir = tmplDir + "user/"
+	testDir = tmplDir + "test/"
 
 	simFrag = simDir + "simulator.frag.html"
 	comFrag = simDir + "comments.frag.html"
@@ -36,6 +37,9 @@ var (
 		"simulator/kinematics": template.Must(template.ParseFiles(base, comFrag, simFrag, simDir+"kinematics.html")),
 		"user/simulations":     template.Must(template.ParseFiles(base, userDir+"simulations.html")),
 		"user/profile":         template.Must(template.ParseFiles(base, userDir+"profile.html")),
+
+		"test/sandbox":         template.Must(template.ParseFiles(testDir + "SandboxRunner.html")),
+		"test/kinematics":      template.Must(template.ParseFiles(testDir + "KinematicsRunner.html")),
 	}
 )
 
---
+++ controllers/test/example_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+  "testing"
+  "reflect"
+)
+
+func TestAverage(t *testing.T) {
+  var v float64
+  v = 1.5
+  if v != 1.5 {
+    t.Error("Expected 1.5, got ", v)
+  }
+} 
