a778e5ede696537fd4686dc04d4734a76604f210
Sam Olds <samolds@gmail.com>
2016-03-21 15:31:41 -0600

    updated shrug and rocket pngs and added tooltip to origin

--- static/js/simulator/ui-controller.js
+++ static/js/simulator/ui-controller.js
@@ -391,9 +391,8 @@ function addKeyframe(){
       $($(li).children()[0]).addClass("input-field-variable");
       $(li).append(
       "<div class=\"input-field-unknown-container\" title=\"Mark this value as unknown.\">" +
-        "<a class=\"input-field-unknown btn green accent-1\"><img src=\"/static/img/toolbox/shrug.png\" height=\"42\" width=\"30\"/></a>" +
+        "<a class=\"input-field-unknown btn green accent-1\"><img class=\"clickable responsive-img\" src=\"/static/img/toolbox/shrug.png\" width=\"30\"/></a>" +
       "</div>");
-      
     }
     
     $('.input-field-unknown').on("click", function(event){
